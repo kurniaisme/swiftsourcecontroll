c017839d42bbe44f54c6ded975bec17149b28c2b
Kurnia Ismi Sholihati <kurniais214@gmail.com>
2026-05-17 09:21:00 +0700

    Initial Commit

---
+++ swiftsourcecontroll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftsourcecontroll
+//
+//  Created by Kurnia Ismi Sholihati on 17/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftsourcecontroll/swiftsourcecontrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftsourcecontrollApp.swift
+//  swiftsourcecontroll
+//
+//  Created by Kurnia Ismi Sholihati on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftsourcecontrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
