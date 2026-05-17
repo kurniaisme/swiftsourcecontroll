6f43ecd4efcae553e376fabf8ac4d63e5efca9e9
Kurnia Ismi Sholihati <kurniais214@gmail.com>
2026-05-17 10:21:42 +0700

    add home view

---
+++ swiftsourcecontroll/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  swiftsourcecontroll
+//
+//  Created by Kurnia Ismi Sholihati on 17/05/26.
+//
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
