9203c5df5a35df90aaaa403bce168ccd6deccfa6
Kurnia Ismi Sholihati <kurniais214@gmail.com>
2026-05-17 10:15:48 +0700

    my first commit

--- swiftsourcecontroll/ContentView.swift
+++ swiftsourcecontroll/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Kurnia Ismi Sholihati")
+                .fontWeight(.bold)
         }
         .padding()
     }
